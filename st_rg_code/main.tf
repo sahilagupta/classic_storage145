13f1da83503824b2aa49c67d319fe513fbb31795
sahilagupta <sahil.a.gupta145@gmail.com>
2025-08-02 17:52:56 +0530

    Update main.tf

--- st_rg_code/main.tf
+++ st_rg_code/main.tf
@@ -1,19 +1,9 @@
 
-resource "azurerm_resource_group" "rg55" {
-  name     = "rg-stg55"
+resource "azurerm_resource_group" "rg555" {
+  name     = "rg-stg555"
   location = "east us"
 }
 
 
 
 
-resource "azurerm_storage_account" "stg55" {
-  name                     = "ramstg55"
-  resource_group_name      = azurerm_resource_group.rg55.name
-  location                 = "east us"
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
- 
-
- 
-  }
